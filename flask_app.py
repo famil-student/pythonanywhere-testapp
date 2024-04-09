dd5498de2a35f1815d77d1167100017b447f6468
Famil Gayibov <famil.gayibov@accolade-pro.com>
2024-04-09 23:51:20 +0200

    ci cd

--- flask_app.py
+++ flask_app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import git
 
 app = Flask(__name__)
 
@@ -12,6 +13,16 @@ def calculator():
         except:
             result = "Error"
     return render_template('index.html', result=result)
+
+@app.route('/update_server', methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        repo = git.Repo('https://github.com/famil-student/pythonanywhere-testapp.git')
+        origin = repo.remotes.origin
+        origin.pull()
+        return 'Updated PythonAnywhere successfully', 200
+    else:
+        return 'Wrong event type', 400
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
